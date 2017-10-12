8ecf67bd26fbe2c1397ad70d641d3144a1e47e0b
Kayla Hansen <shiverr@gmail.com>
2017-10-12 01:17:59 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,15 +1,16 @@
 # Write your code here.
-katz_deli []
 
 def line
-  new_array = []
-  if array.length > 1
-    array.each_with_index do |name, line|
+  katz_deli = []
+  if katz_deli.length > 1
+    katz_deli.each_with_index do |name, line|
     lineplus1 = line + 1
     output = puts "The line is currently: #{lineplus1} #{name}"
-    new_array << output
+    katz_deli << output
   else
     puts "The line is currently empty."
 end
 end
+return katz_deli
 end
+
