4fd41fc0473bd0c8ecc0c6c995bcb8cdc4e820ac
Kayla Hansen <shiverr@gmail.com>
2017-10-12 01:10:34 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -5,7 +5,7 @@ def line
   new_array = []
   if array.length > 1
     array.each_with_index do |name, line|
-      lineplus1 = line + 1
+    lineplus1 = line + 1
     output = puts "The line is currently: #{lineplus1} #{name}"
     new_array << output
   else
@@ -14,4 +14,3 @@ def line
 end
   return new_array
 end
-
