42523b84eb8b8850e0e6c6f1a0301ae7821c70ea
Kayla Hansen <shiverr@gmail.com>
2017-10-12 01:39:11 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -11,7 +11,7 @@ end
 end
 
 def take_a_number(katz_deli, name)
-  if deli.empty?
+  if katz_deli.empty?
   current_line = []
   katz_deli.each.with_index(1) do |name, position|
   current_line << "Welcome, #{name}. You are number #{position} in line."
